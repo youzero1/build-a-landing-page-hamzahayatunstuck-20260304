08c4037e37d14554982d3612be4db6009baa4caf
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-04 15:39:52 +0500

    Add lib/entities/Contact.ts

---
+++ lib/entities/Contact.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('contacts')
+export class Contact {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column('text')
+  message: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
